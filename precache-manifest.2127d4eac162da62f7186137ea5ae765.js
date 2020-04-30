338783127876e66af36b40bcb247e8b6c1e5c9a2
Artem Bondarenko <artem.arturovich.bondarenko@gmail.com>
2020-04-30 23:19:15 +0300

    Updates

---
+++ precache-manifest.2127d4eac162da62f7186137ea5ae765.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "858804b8ffb596edefb0",
+    "url": "/goit-react-hw-02-feedback/static/js/main.26875213.chunk.js"
+  },
+  {
+    "revision": "d210b4c77610b7c74f44",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime~main.e1265419.js"
+  },
+  {
+    "revision": "3f03175b3c89cae4abd6",
+    "url": "/goit-react-hw-02-feedback/static/js/2.b74005f8.chunk.js"
+  },
+  {
+    "revision": "a4a2357722712d81e694bb3738a86e3c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  }
+];
